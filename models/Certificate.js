d61d85087a9cd45110c2ba8bd5414cbe794057a8
smsvedang <sonivedang7@gmail.com>
2025-12-15 09:16:50 +0530

    update certificate error

---
+++ models/Certificate.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const certificateSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    mobile: String,
+    courseId: String,
+    courseTitle: String,
+    language: String,
+    certificateId: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Certificate', certificateSchema);
