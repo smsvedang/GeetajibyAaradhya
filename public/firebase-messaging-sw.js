bd5a99762420a0315a3429de7de766eb472ebe32
smsvedang <sonivedang7@gmail.com>
2026-01-10 17:23:30 +0530

    update notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,9 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+   apiKey: "AIzaSyCmAzjIpbPy1ENyiLoURLWg2cI7fBWcUa4",
+  authDomain: "warrior-aaradhya.firebaseapp.com",
+  projectId: "warrior-aaradhya",
+  storageBucket: "warrior-aaradhya.firebasestorage.app",
+  messagingSenderId: "105939562180",
+  appId: "1:105939562180:web:6eb2d862a01902a98fe391",
+  measurementId: "G-Y4KCBXN8Y4"
 });
 
 const messaging = firebase.messaging();
